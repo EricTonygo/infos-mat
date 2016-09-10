bf8873a77ab6e06cf243b774d977c0a924940e86
EricTonygo <erictonyelissouck@yahoo.fr>
2016-09-10 15:57:27 +0100

    mise à jour

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new NNGenie\InfosMatBundle\NNGenieInfosMatBundle(),
             new NNGenie\UserBundle\NNGenieUserBundle(),
 			new FOS\UserBundle\FOSUserBundle(),
+			new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
