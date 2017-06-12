f6736c2dd9637af06123a85f36dd953995f3f4d8
EricTonygo <tonye.eric@gmail.com>
2017-06-12 17:16:03 +0200

    Mise à jour de ExcelService pour gerer les valeurs creuses dans le fichier excel

--- src/NNGenie/InfosMatBundle/Controller/ImportDatabaseController.php
+++ src/NNGenie/InfosMatBundle/Controller/ImportDatabaseController.php
@@ -26,30 +26,33 @@ class ImportDatabaseController extends Controller {
 //            $file = $uploadedFile->move($this->getUploadRootDir(), $name);
 //        }
         if (!$this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-          return $this->redirect($this->generateUrl('fos_user_security_login'));
-          } 
+            return $this->redirect($this->generateUrl('fos_user_security_login'));
+        }
         if ($request->isMethod("POST")) {
             $file = $request->files->get('materiels_xlsx_file');
 
             if ($file) {
                 //$original_filename = $file->getClientOriginalName();
-                $name = 'materiels.' . $file->guessExtension();
-                $file = $file->move($this->getUploadRootDir(), $name);
-                $result = "tmp";
-                if(file_exists($this->getUploadRootDir() ."/". $name)){
-                    $result = $this->get('excel_service')->importMaterielsToDatabase($this->getUploadRootDir() ."/". $name);
+                if (strtolower($file->guessExtension()) === "xls" || strtolower($file->guessExtension()) === "xlsx" || strtolower($file->guessExtension()) === "csv") {
+                    $name = "materiels"."." . $file->guessExtension();
+                    $file = $file->move($this->getUploadRootDir(), $name);
+                    $result = "tmp";
+                    if (file_exists($this->getUploadRootDir() . "/" . $name)) {
+                        $result = $this->get('excel_service')->importMaterielsToDatabase($this->getUploadRootDir() . "/" . $name);
+                    }
+                    $message = $this->get('translator')->trans('Materiels.imported_success', array(), "NNGenieInfosMatBundle");
+                    $this->get('ras_flash_alert.alert_reporter')->addSuccess($message);
+                    return $this->redirect($this->generateUrl('nn_genie_infos_mat_materiels'));
+                } else {
+                    $message = $this->get('translator')->trans('Veuillez selectionner un fichier excel au format xls, xlsx ou csv', array(), "NNGenieInfosMatBundle");
+                    $this->get('ras_flash_alert.alert_reporter')->addError($message);
                 }
-                $message = $this->get('translator')->trans('Materiels.imported_success', array(), "NNGenieInfosMatBundle");
-                $this->get('ras_flash_alert.alert_reporter')->addSuccess($message);
-                return $this->redirect($this->generateUrl('nn_genie_infos_mat_materiels'));
-            }else{
+            } else {
                 $message = $this->get('translator')->trans('Materiels.imported_failure', array(), "NNGenieInfosMatBundle");
                 $this->get('ras_flash_alert.alert_reporter')->addError($message);
-                return $this->render('NNGenieInfosMatBundle:importDatabase:form-import-database.html.twig');
             }
-        } elseif ($request->isMethod("GET")) {
-            return $this->render('NNGenieInfosMatBundle:importDatabase:form-import-database.html.twig');
         }
+        return $this->render('NNGenieInfosMatBundle:importDatabase:form-import-database.html.twig');
     }
 
     private function getUploadRootDir() {
